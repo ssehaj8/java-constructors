34d802f877ff5b61bbf1f6ea360677df1203f675
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-12 01:19:50 +0530

    [Sehajpreet] Add:Person class with a copy constructor that clones another person's attributes.

--- HotelBooking.java
+++ HotelBooking.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+class HotelBooking {
+    private String guestName;
+    private String roomType;
+    private int nights;
 
-public class HotelBooking {
+    public HotelBooking() {
+        this.guestName = "Unknown";
+        this.roomType = "Standard";
+        this.nights = 1;
+    }
+
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    public HotelBooking(HotelBooking other) {
+        this.guestName = other.guestName;
+        this.roomType = other.roomType;
+        this.nights = other.nights;
+    }
+
+    public void display() {
+        System.out.println("Guest Name: " + guestName + ", Room Type: " + roomType + ", Nights: " + nights);
+    }
+
+    public static void main(String[] args) {
+        HotelBooking booking1 = new HotelBooking("Sehajpreet", "Deluxe", 2);
+        HotelBooking booking2 = new HotelBooking(booking1);
+        HotelBooking booking3 = new HotelBooking();
+
+        booking1.display();
+        booking2.display();
+        booking3.display();
+    }
 }
