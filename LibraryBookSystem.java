54b2ca7d3fac454b41b7d9b44201fbe2c51dfca0
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-12 01:21:11 +0530

    [Sehajpreet] Add: Create a Book class with attributes title, author, price, and availability. Implement a method to borrow a book.

--- LibraryBookSystem.java
+++ LibraryBookSystem.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+class LibraryBookSystem {
+    private String title;
+    private String author;
+    private double price;
+    private boolean isAvailable;
 
-public class LibraryBookSystem {
+
+    public LibraryBookSystem(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.isAvailable = true; // Book is available by default
+    }
+
+    public boolean borrowBook() {
+        if (isAvailable) {
+            isAvailable = false;
+            System.out.println("You have successfully borrowed: " + title);
+            return true;
+        } else {
+            System.out.println("Sorry, the book '" + title + "' is currently unavailable.");
+            return false;
+        }
+    }
+
+    public void returnBook() {
+        isAvailable = true;
+        System.out.println("You have returned: " + title);
+    }
+
+
+    public void displayBookInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+        System.out.println("Availability: " + (isAvailable ? "Available" : "Not Available"));
+    }
+
+    public static void main(String[] args) {
+        LibraryBookSystem book1 = new LibraryBookSystem("The Great Gatsby", "F. Scott Fitzgerald", 10.99);
+        book1.displayBookInfo();
+        book1.borrowBook();
+        book1.displayBookInfo();
+        book1.returnBook();
+        book1.displayBookInfo();
+    }
 }
