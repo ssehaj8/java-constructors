0cd54ff23187b663053882ec53c19cf8055c56e4
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-11 11:17:47 +0530

    [Sehajpreet] Add:  Circle class with a radius attribute

---
+++ Circle.java
@@ -0,0 +1,38 @@
+class Circle {
+    double radius;
+
+
+    public Circle() {
+        this(1.0);
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+
+    public void display() {
+        System.out.println("Radius: " + radius);
+        System.out.println("Area: " + getArea());
+        System.out.println("Circumference: " + getCircumference());
+        System.out.println("-------------------------");
+    }
+
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        circle1.display();
+
+
+        Circle circle2 = new Circle(5.0);
+        circle2.display();
+    }
+}
