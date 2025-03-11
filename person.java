fbf0e7542c08803c09f2dad49b3719170a24f162
ssehaj8 <sehajpreetkaur0812@gmail.com>
2025-03-12 01:22:57 +0530

    [Sehajpreet] Add:Create a CarRental class with attributes and Add constructors to initialize the rental details and calculate total cost.

--- person.java
+++ person.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
+class Person {
+    private String name;
+    private int age;
 
-public class person {
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(Person other) {
+        this.name = other.name;
+        this.age = other.age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+
+    public static void main(String[] args) {
+        Person person1 = new Person("Sehaj", 22);
+        Person person2 = new Person(person1);
+
+        person1.display();
+        person2.display();
+    }
 }
+
